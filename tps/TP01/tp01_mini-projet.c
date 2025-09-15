9fcbdb524877820d308b35c454a1338532a4013a
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-09-15 12:28:34 +0200

    Add files via upload

---
+++ tps/TP01/tp01_mini-projet.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+
+// icônes pour affichage (à insérer comme un caractère)
+// 🌡️📊🧪🚿🆔📁🔍🛑
+
+int main() {
+    float TempEau1;
+    float tempEau2;
+    float tempZone1;
+    float tempZone2;
+    unsigned int pression;
+    unsigned int debit;
+
+    unsigned short numDiag;
+    unsigned short idSonde;
+    char cheminLog[50];
+
+    // Déclaration des variables
+    // Utilisez les types appropriés pour chaque variable
+    // Notez que les variables doivent être déclarées avant leur utilisation
+
+
+
+    // Demande à l'utilisateur d'entrer les valeurs
+    // Utilisez les formattages appropriés pour chaque variable
+
+
+
+    // Calcul de la température moyenne des eaux de refroidissement
+
+
+
+    // Calcul de la température moyenne des zones
+ 
+
+
+    // Calcul de l'écart de température des eaux
+
+
+
+    // Calcul de l'écart de température des zones
+
+
+
+    // Conversion de la pression en Pa
+
+
+
+    // Calcul du niveau de débit en %
+
+
+
+    // Incrémentation du numéro de diagnostic
+
+
+    
+    // Détermination de la parité du numéro de sonde
+
+
+
+// Affichage formaté
+    printf("\n-----------------------------------------------------\n");
+    printf("          🔍 DIAGNOSTIC DU SYSTEME SENTINEL\n");
+    printf("-----------------------------------------------------\n");
+    
+
+
+
+
+    return 0;
+}
