b4e69d1a8935f1b95a2eb11e84a4c5b47e400eb9
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-11-03 11:41:13 +0100

    Add files via upload

--- tps/TP04/exercice_4.c
+++ tps/TP04/exercice_4.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h> // Pour les fonctions rand() et srand()
-#include <time.h>  // pour la fonction time() 
+#include <stdlib.h>
+#include <time.h>  
 
 int main() {
-    srand(time(0)); // Initialise le générateur de nombres aléatoires
-    int nombreAdeviner = rand() % 100 + 1; // Nombre aléatoire entre 1 et 100
+    srand(time(0));
+    int nombreAdeviner = rand() % 100 + 1;
 
-    // Écrire ici le code pour demander à l'utilisateur de deviner
+    int correct = 0;
+    int tryNbr = 0;
+
+    int GuessNbr = 0;
+
+    while (correct != 1)
+    {
+        printf("donnez un nombre guess : \n");
+        scanf("%d", &GuessNbr);
+        
+        if (GuessNbr < nombreAdeviner){
+            printf("Le nombre est trop petit \n");
+            tryNbr++;
+        }else if (GuessNbr > nombreAdeviner){
+            printf("Le nombre est trop grand \n");
+            tryNbr++;
+        }else if (GuessNbr = nombreAdeviner){
+            printf("Bravo vous avez trouver %d, vous avez tenter %d d'essai\n" , nombreAdeviner, tryNbr);
+            correct++;
+        }else {
+            printf("Une erreur est survenu \n");
+        }
+    }
+    
 
     return 0;
 }
