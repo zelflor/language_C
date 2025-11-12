bcec1bb9682040bed0301af552e4b4435fb0613b
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-11-12 16:43:31 +0100

    Add files via upload

--- tps/TP07/exercice_01.c
+++ tps/TP07/exercice_01.c
@@ -2,11 +2,19 @@
 
 int main(){
 
-
+    int nbrRow = 1;
+    printf("    0    1    2    3    4    5    6    7    8    9   10\n    0");
     for (int i = 0; i <= 127; i++){
-        printf("%5d", i);
-        if (i % 9 == 0 && i != 0) {
+        if (i <= 31){
+            printf("    X");
+        }else {
+            printf("%5c", i);
+        }
+        
+        if (i % 10 == 0 && i != 0) {
             printf("\n");
+            printf("%5d", nbrRow);
+            nbrRow++;
         }
         
         
