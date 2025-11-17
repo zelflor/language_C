8f932b94f8499cbaba54e7007e8f4184b3bddf0b
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-11-17 11:32:50 +0100

    Add files via upload

--- tps/TP07/exercice_01.c
+++ tps/TP07/exercice_01.c
@@ -2,30 +2,45 @@
 
 int main(){
 
-    int nbrRow = 1;
+    int nbrRow = 0;
     int nbrColumn = 0;
-    while (nbrColumn <= 10)
+    int nbrMax = 0;
+    do {
+        printf("Saisir un nombre entre 0 et 127: ");
+        scanf("%d", &nbrMax);
+    }while (nbrMax < 0 || nbrMax > 127);
+    
+    printf("     ");
+    while (nbrColumn <= 9)
     {
+        
         printf("%5d", nbrColumn);
         nbrColumn++;
     }
-    printf("\n    0");
-    
-    
-    for (int i = 0; i <= 127; i++){
+    printf("\n");
+    printf("%5d", nbrRow);
+    nbrRow += 10;
+    int i = 0;
+    for (int j = 1; j <= nbrMax; j++){
+
         if (i <= 31){
             printf("    X");
         }else {
             printf("%5c", i);
+            
         }
+        // printf("%2d", i);
         
-        if (i % 10 == 0 && i != 0) {
+        if (j % 10 == 0 && j != 0) {
+            
             printf("\n");
             printf("%5d", nbrRow);
-            nbrRow++;
+            nbrRow += 10;
+            
+
         }
         
-        
+    i++;
     }
     return 0;
 }
