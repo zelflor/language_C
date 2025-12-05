c26dad74cc9e4436736e79c0c361760e6ea36329
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-12-05 17:14:56 +0100

    Add files via upload

--- tps/TP09/exercice_03.c
+++ tps/TP09/exercice_03.c
@@ -2,40 +2,45 @@
 
 int main() {
 
-
+    
     char continuer = 'o';
-
+    
     while (continuer != 'n')
     {
-        char mot[20] = { 0 };
-        
-        printf("\nSaisir un mot : ");
-        scanf("%s", mot);
 
-        int nbrMaxTaille = 0;
-        for (int y = 0; y < 29; y++){
-            if (mot[y] != 0){
-                nbrMaxTaille++;
+        printf("Saisir un mot :");
+        int palindrome = 1;
+        char mot[100];
+        scanf(" %99s", mot);
+
+        int taillemot = 0;
+        for (int i = 0; mot[i] != '\0'; i++){
+            taillemot++;
+        }
+
+        int finmot = taillemot - 1;
+        int startmot = 0;
+
+
+        while (startmot < finmot){
+            if (mot[startmot] != mot[finmot]){
+                palindrome = 0;
+                break;
+            }else {
+                startmot++;
+                finmot--;
             }
-            
         }
-        char reversemot[20] = { 0 };
-
-        for (int i = 19; i >= 0; i--){
-            int z = 0;
-            reversemot[z] = mot[i];
-
-            z++;
+        printf("\n");
+        if (palindrome == 1){
+            printf("%s est un palindrome", mot);
+        }else {
+            printf("%s n'est pas un palindrome", mot);
         }
-        printf("%s", reversemot);
-        printf("%s", mot);
-
-
-
 
 
         printf("\nVoulez-vous continuer (o/n) : ");
-        scanf("%c", &continuer);
+        scanf(" %c", &continuer);
     }
     
 
