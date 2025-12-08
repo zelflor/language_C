1d972408512e6b9bdfd85aeaf11bae5a5e896ea0
ZELFLOR <139008962+zelflor@users.noreply.github.com>
2025-12-08 12:02:31 +0100

    Add files via upload

--- tps/TP09/exercice_02.c
+++ tps/TP09/exercice_02.c
@@ -5,14 +5,14 @@ int main() {
     char saisie[30] = { 0 };
 
     printf("Saisir une chaine de caracteres : ");
-    scanf("%s", &saisie);
+    scanf("%s", saisie);
     printf("La chaine saisie est \"%s\"", saisie);
     printf("\n-----------------------------------------------------------\n");
 
 
     printf("AFFICHAGE CARACTERE PAR CARACTERE DE LA CHAINE\n");
     printf("Saisir une chaine de caracteres : ");
-    scanf("%s", &saisie);
+    scanf("%s", saisie);
     for (int i = 0; i <= 10; i++){
         printf("%c ", saisie[i]);
     }
@@ -21,16 +21,14 @@ int main() {
 
     printf("AFFICHER LE NOMBRE DE CARACTERES DANS UNE CHAINE\n");
     printf("Saisir une chaine de caracteres : ");
-    scanf("%s", &saisie);
-    int nbrMaxTaille = 0;
-    for (int y = 0; y < 29; y++){
-        if (saisie[y] != 0){
-            nbrMaxTaille++;
-        }
-        
+    scanf("%s", saisie);
+    int nbrTaille = 0;
+    while (saisie[nbrTaille] != '\0'){
+        nbrTaille++;
     }
+    
 
-    printf("La chaine \"%s\" a %d caracteres", saisie, nbrMaxTaille);
+    printf("La chaine \"%s\" a %d caracteres", saisie, nbrTaille);
     printf("\n-----------------------------------------------------------\n");
 
 
@@ -39,31 +37,64 @@ int main() {
     char chaine2[10];
     printf("SAISIR DEUX CHAINES ET CONCATENER CHAINE2 A CHAINE1\n");
     printf("Saisir chaine 1 :");
-    scanf("%s", &chaine1);
+    scanf("%9s", chaine1);
     printf("Saisir chaine 2 :");
-    scanf("%s", &chaine2);
+    scanf("%9s", chaine2);
 
-    printf("La concatenation de Chaine1 + Chaine2 donne \"%s%s\" ", chaine1, chaine2);
+    int nbrTaillechaine1 = 0;
+    while (chaine1[nbrTaillechaine1] != '\0'){
+        nbrTaillechaine1++;
+    }
+
+    int nbrTaillechaine2 = 0;
+    while (chaine1[nbrTaillechaine2] != '\0'){
+        nbrTaillechaine2++;
+    }
+
+    int nbrTaillechaines = nbrTaillechaine1 + nbrTaillechaine2;
+    
+    char chaines[nbrTaillechaines];
+    
+    
+    for (int i = 0; i < nbrTaillechaines; i++ ){
+        if (i >= nbrTaillechaine1){
+            chaines[i] = chaine2[i  - nbrTaillechaine1];
+        }else {
+            chaines[i] = chaine1[i];
+            
+        }
+        
+    }
+    
+    
+
+    printf("La concatenation de Chaine1 + Chaine2 donne \"%s\" ", chaines);
     
 
     printf("\n-----------------------------------------------------------\n");
     
 
+
+
     printf("SAISIR UNE CHAINE ET L'AFFICHER A L'ENVERS\n");
     printf("Saisir une chaine de caracteres : ");
-    scanf("%s", &saisie);
-    nbrMaxTaille = 0;
+     char saisieenvers[30] = { 0 };
+    scanf("%29s", &saisieenvers);
+    int nbrMaxTaille = 0;
     for (int z = 0; z < 29; z++){
-        if (saisie[z] != 0){
+        if (saisieenvers[z] != 0){
             nbrMaxTaille++;
         }
         
     }
     for (int a = nbrMaxTaille; a >= 0; a--){
-        printf("%c", saisie[a]);
+        printf("%c", saisieenvers[a]);
     }
 
     printf("\n-----------------------------------------------------------\n");
+
+
+
     printf("SAISIR UNE CHAINE ET COMPTER LE OCCURENCES DE LA LETTRE 'a' DANS LA CHAINE\n");
     printf("Saisir une chaine de caracteres : ");
     scanf("%s", &saisie);
