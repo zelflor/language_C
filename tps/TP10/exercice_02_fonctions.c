1a37d9bed475c58efae4a0e1fa65dcb4f7982f8c
Zelflor (Nairukki) <139008962+zelflor@users.noreply.github.com>
2025-12-16 08:50:17 +0100

    Add files via upload

--- tps/TP10/exercice_02_fonctions.c
+++ tps/TP10/exercice_02_fonctions.c
@@ -3,6 +3,79 @@
 
 
 
+void displayChaine(char param[]);
+
+int GetTaille(char param[]);
+
+void displayCaractere(char param[], int tailleparam);
+
+void displayconcatener(char param1[], char param2[]);
+
+void displayChainereverse(char param[]);
+
+int compteroccurences(char param[], char lettre);
+
+int main(){
+
+
+
+    char saisie[30];
+    printf("\nSaisir une chaine de caracteres :");
+    scanf("%s", saisie);
+    displayChaine(saisie);
+
+    printf("-----------------------------------------------------------\n");
+    printf("AFFICHAGE CARACTERE PAR CARACTERE DE LA CHAINE\n");
+    printf("Saisir une chaine de caracteres :");
+    scanf("%s", saisie);
+    int taillesaisie = GetTaille(saisie);
+    displayCaractere(saisie, taillesaisie );
+    
+    printf("\n-----------------------------------------------------------\n");
+    printf("AFFICHER LE NOMBRE DE CARACTERES DANS UNE CHAINE\n");
+    printf("Saisir une chaine de caracteres : ");
+    scanf("%s", saisie);
+    taillesaisie = GetTaille(saisie);
+    printf("\nLa chaine \"%s\" a %d caracteres", saisie, taillesaisie);
+
+
+    printf("\n-----------------------------------------------------------\n");
+    printf("SAISIR DEUX CHAINES ET CONCATENER CHAINE2 A CHAINE1\n");
+    char chaine1[30];
+    char chaine2[30];
+    printf("Saisir chaine 1 : ");
+    scanf("%s", chaine1);
+
+    printf("Saisir chaine 2 : ");
+    scanf("%s", chaine2);
+
+    printf("La concatenation de %s + %s donne \"", chaine1, chaine2);
+    displayconcatener(chaine1, chaine2);
+    printf("\"");
+
+
+
+    printf("\n-----------------------------------------------------------\n");
+    printf("SAISIR UNE CHAINE ET L'AFFICHER A L'ENVERS\n");
+    printf("Saisir une chaine de caracteres : ");
+    scanf("%s", saisie);
+    displayChainereverse(saisie);
+    
+    printf("-----------------------------------------------------------\n");
+    printf("SAISIR UNE CHAINE ET COMPTER LE OCCURENCES DE LA LETTRE 'a' DANS LA CHAINE \n");
+    printf("Saisir une chaine de caracteres : ");
+    scanf("%s", saisie);
+    char lettre = 'a';
+    int nbroccurences = compteroccurences(saisie, lettre);
+    printf("Il y a %d lettre(s) '%c' dans \"%s\"", nbroccurences, lettre, saisie);
+
+    return 0;
+}
+
+
+
+
+
 void displayChaine(char param[]){
     printf("La chaine saisie est \"%s\"  \n", param);
 }
@@ -81,61 +154,4 @@ int compteroccurences(char param[], char lettre){
             
     }
     return nbrlettreTaille;
-}
-
-int main(){
-
-
-
-    char saisie[30];
-    printf("\nSaisir une chaine de caracteres :");
-    scanf("%s", saisie);
-    displayChaine(saisie);
-
-    printf("-----------------------------------------------------------\n");
-    printf("AFFICHAGE CARACTERE PAR CARACTERE DE LA CHAINE\n");
-    printf("Saisir une chaine de caracteres :");
-    scanf("%s", saisie);
-    int taillesaisie = GetTaille(saisie);
-    displayCaractere(saisie, taillesaisie );
-    
-    printf("\n-----------------------------------------------------------\n");
-    printf("AFFICHER LE NOMBRE DE CARACTERES DANS UNE CHAINE\n");
-    printf("Saisir une chaine de caracteres : ");
-    scanf("%s", saisie);
-    taillesaisie = GetTaille(saisie);
-    printf("\nLa chaine \"%s\" a %d caracteres", saisie, taillesaisie);
-
-
-    printf("\n-----------------------------------------------------------\n");
-    printf("SAISIR DEUX CHAINES ET CONCATENER CHAINE2 A CHAINE1\n");
-    char chaine1[30];
-    char chaine2[30];
-    printf("Saisir chaine 1 : ");
-    scanf("%s", chaine1);
-
-    printf("Saisir chaine 2 : ");
-    scanf("%s", chaine2);
-
-    printf("La concatenation de %s + %s donne \"", chaine1, chaine2);
-    displayconcatener(chaine1, chaine2);
-    printf("\"");
-
-
-
-    printf("\n-----------------------------------------------------------\n");
-    printf("SAISIR UNE CHAINE ET L'AFFICHER A L'ENVERS\n");
-    printf("Saisir une chaine de caracteres : ");
-    scanf("%s", saisie);
-    displayChainereverse(saisie);
-    
-    printf("-----------------------------------------------------------\n");
-    printf("SAISIR UNE CHAINE ET COMPTER LE OCCURENCES DE LA LETTRE 'a' DANS LA CHAINE \n");
-    printf("Saisir une chaine de caracteres : ");
-    scanf("%s", saisie);
-    char lettre = 'a';
-    int nbroccurences = compteroccurences(saisie, lettre);
-    printf("Il y a %d lettre(s) '%c' dans \"%s\"", nbroccurences, lettre, saisie);
-
-    return 0;
 }
